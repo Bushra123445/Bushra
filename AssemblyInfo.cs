8696118466c1c5b00d42c424211b6c9343403f5b
Bushra123445 <65974219+Bushra123445@users.noreply.github.com>
2023-01-08 14:34:30 +0500

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("bushraaaa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("bushraaaa")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c1e18f6-5183-42c6-8cbc-617251538f35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace bushraaaa
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            {
+                WebDriver driver = new ChromeDriver();
+                driver.Url = "https://adactinhotelapp.com/";
+
+                //driver.FindElement(By.Id("username")).SendKeys("AmirTester");
+                //var element = driver.FindElement(By.Id("username"));
+                //element.SendKeys(Keys.Tab);
+                //driver.FindElement(By.Id("login")).Click();
+                Thread.Sleep(5000);
+
+                Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+                screenshot.SaveAsFile(@"C:\Users\Bushra Rafiq\source\repos\bushraaaa", ScreenshotImageFormat.Png);
+              //  screenshot.SaveAsFile(@".\\Screenshot.png", ScreenshotImageFormat.Png);
+
+
+                driver.Quit();
+            }
+        }
+    }
+}
+
